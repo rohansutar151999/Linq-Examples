38b479150457dd68165a04a1f9c8423b3910af02
hp <hp@DESKTOP-ATI935J>
2025-06-18 22:29:59 +0530

    Add project files.

---
+++ Linq Examples/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Linq_Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 5, 12, 15, 7, 3, 20 };
+
+            //Linq method system
+            //var result = from n in numbers
+            //             where n > 10
+            //             select n;
+
+            var result = numbers.Where(n => n > 10);
+
+            foreach (var n in result) 
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
